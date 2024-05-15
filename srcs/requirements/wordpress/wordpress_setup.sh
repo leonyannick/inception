d89073f459562c069de514c13a80fb78b4ec768e
Leon <leon.yannik@web.de>
2024-05-15 17:35:21 +0200

    cleanup wordpress_setup.sh

--- srcs/requirements/wordpress/wordpress_setup.sh
+++ srcs/requirements/wordpress/wordpress_setup.sh
@@ -1,40 +1,41 @@
 #!/bin/bash
 
-#change working dir to where all the wordpress files are
-#cd /var/www/wordpress
-
 # give MariaDB time to lauch correctly:
-while ! mysqladmin ping --host=$DB_HOST --silent; do
+while ! mysqladmin ping --host=$DB_HOST --port=$DB_HOST_PORT --silent; do
 	echo "$(date +%d%m%y\ %H:%M:%S) Waiting for database connection..."
 	sleep 5
 done
 
-echo "connection succesfull"
+echo "Connection to $DB_HOST succesfull"
 
-wp core download --allow-root
 
-echo "config create --allow-root --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$(cat $SQL_USER_PW_FILE) --dbhost=$DB_HOST:$DB_HOST_PORT"
+if ! wp core is-installed --allow-root; then
+    wp core download --allow-root
 
-wp config create --allow-root \
-				--dbname=$SQL_DATABASE \
-				--dbuser=$SQL_USER \
-				--dbpass=$(cat $SQL_USER_PW_FILE) \
-				--dbhost=$DB_HOST:$DB_HOST_PORT
+    if [ ! -f wp-config.php ]; then
+        wp config create --allow-root \
+                        --dbname=$SQL_DATABASE \
+                        --dbuser=$SQL_USER \
+                        --dbpass=$(cat $SQL_USER_PW_FILE) \
+                        --dbhost=$DB_HOST:$DB_HOST_PORT
+    fi
 
-# echo "--url=$DOMAIN_NAME  --title=$SITE_TITLE   --admin_user=$WP_ADMIN   --admin_password=$(cat $WP_ADMIN_PW_FILE)   --admin_email=$WP_ADMIN_EMAIL   --allow-root"
+	#:8443 only needed for localhost. can later be removed in the vm with the right domain: lbaumann.42.fr
+    wp core install --url=$DOMAIN_NAME:8443 \
+                    --title=$SITE_TITLE \
+                    --admin_user=$WP_ADMIN \
+                    --admin_password=$(cat $WP_ADMIN_PW_FILE) \
+                    --admin_email=$WP_ADMIN_EMAIL \
+                    --allow-root
+	
+	# create a new wordpress user with the credentials from the .env
+	wp user create --allow-root $WP_USER $WP_USER_EMAIL \
+					--role=editor \
+					--user_pass=$(cat $WP_USER_PW_FILE) \
 
-wp core install --url=localhost:8443 \
-							  --title=$SITE_TITLE \
-							  --admin_user=$WP_ADMIN \
-							  --admin_password=$(cat $WP_ADMIN_PW_FILE) \
-							  --admin_email=$WP_ADMIN_EMAIL \
-							  --allow-root
+fi
 
-# # create a new wordpress user with the credentials from the .env
-# wp user create --allow-root $WP_USER \
-# 							$WP_USER_EMAIL \
-# 							--role=editor \
-# 							--user_pass=$(cat $WP_USER_PW_FILE) \
+
 
 # # create dir if not existant
 if [ ! -d /run/php ]; then
